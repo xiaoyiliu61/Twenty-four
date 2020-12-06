01d056121fed1f0efb55b40a129cd27347de5196
xiaoyiliu61 <2693596075@qq.com>
2020-12-06 19:01:49 +0800

    Twenty-four“

---
+++ src/com/bit/connect/BTCConnect.java
@@ -0,0 +1,10 @@
+package com.bit.connect;
+
+public class BTCConnect {
+    //maven :管理和构建项目的依赖和配置
+    //依赖的配置：xml文件
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+    
+}
