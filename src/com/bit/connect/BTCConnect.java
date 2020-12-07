fd7541d85a4b7d83b2768ca73682caa28b2d3d85
xiaoyiliu61 <2693596075@qq.com>
2020-12-07 19:59:26 +0800

    BTC

--- src/com/bit/connect/BTCConnect.java
+++ src/com/bit/connect/BTCConnect.java
@@ -1,10 +1,41 @@
 package com.bit.connect;
 
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.Base64;
+
 public class BTCConnect {
+    private static final String RPCUSER = "user";
+    private static final String RPCPASSWORD = "user";
+
+
+
     //maven :管理和构建项目的依赖和配置
     //依赖的配置：xml文件
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        JSONObject object = new JSONObject();
+        object.put("id",System.currentTimeMillis()+"");
+        object.put("jsonrpc","2.0");//rpc服务协议版本
+        object.put("method","getblockcount");
+        //object.put("params","");
+        object.put("Authorization","Basic"+ base64Encode(RPCUSER+"："+RPCPASSWORD));
+        String jsonRpcStr = object.toJSONString();
+        System.out.println(jsonRpcStr);
     }
-    
+
+    private static String base64Encode(String s) {
+        return null;
+    }
+
+
+    /**
+     * 1.准备json-rpc通信的json数据
+     * 2.使用java网络通信进行rpc链接
+     * 3.接收java中的http形式的rpc链接响应
+     * 4.处理结果
+     */
+
+
 }
---
+++ src/com/bit/connect/JSONDemo.java
@@ -0,0 +1,65 @@
+package com.bit.connect;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class JSONDemo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        Student student =new Student();
+        student.setName("隔壁老王");
+        student.setAge(18);
+        student.setSex("male");
+
+        System.out.println("学生姓名:"+student.name);
+        System.out.println("学生年龄:"+student.age);
+        System.out.println("学生性别:"+student.sex);
+
+        //序列化操作
+        JSONObject object = (JSONObject) JSONObject.toJSON(student);
+        String objStr = object.toJSONString();
+        System.out.println(object.toJSONString());
+
+        //反序列化操作
+        Student laowang =JSONObject.parseObject(objStr,Student.class);
+        System.out.println(laowang.name);
+
+        //第二种序列化的方式
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("name","于洪伟");
+        jsonObject.put("sex","男");
+        jsonObject.put("slgan","我变秃了，也变强了");
+        jsonObject.put("languages","go,java,python");
+        String code =jsonObject.toJSONString();
+        System.out.println(code);
+    }
+
+    static class Student{
+        private String name;
+        private int age;
+        private String sex;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public String getSex() {
+            return sex;
+        }
+
+        public void setSex(String sex) {
+            this.sex = sex;
+        }
+    }
+}
